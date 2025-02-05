e328e2bc2cb47a9b0700100f07d1357eb64a88d6
pablo <pablo.diaz@clara.co>
2025-02-04 21:01:48 -0300

    passager WIP

--- src/components/PassagersList.tsx
+++ src/components/PassagersList.tsx
@@ -1,6 +1,7 @@
 import { PassagerTableProps } from '@/types'
 import {
   Box,
+  Button,
   Table,
   TableBody,
   TableCell,
@@ -9,6 +10,7 @@ import {
   TableRow,
   Typography
 } from '@mui/material'
+import Link from 'next/link'
 import { FC } from 'react'
 
 const PassagersList: FC<PassagerTableProps> = async ({ passagers }) => {
@@ -18,6 +20,14 @@ const PassagersList: FC<PassagerTableProps> = async ({ passagers }) => {
         <Typography variant='h6' component='h2'>
           Passagers List
         </Typography>
+        <Link href='/passagers/create' className='ml-auto mr-4' passHref>
+          <Button
+            sx={{ backgroundColor: 'MediumSeaGreen' }}
+            variant='contained'
+          >
+            Add new
+          </Button>
+        </Link>
         <Table aria-label='simple table'>
           <TableHead>
             <TableRow sx={{ '& > th': { fontWeight: 700 } }}>
@@ -27,15 +37,16 @@ const PassagersList: FC<PassagerTableProps> = async ({ passagers }) => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {passagers.map(({ id, name, dni }) => (
-              <TableRow key={name} className='hover:bg-slate-300'>
-                <TableCell component='th' scope='row'>
-                  {id}
-                </TableCell>
-                <TableCell>{name}</TableCell>
-                <TableCell>{dni}</TableCell>
-              </TableRow>
-            ))}
+            {passagers &&
+              passagers.map(({ id, name, dni }) => (
+                <TableRow key={name} className='hover:bg-slate-300'>
+                  <TableCell component='th' scope='row'>
+                    {id}
+                  </TableCell>
+                  <TableCell>{name}</TableCell>
+                  <TableCell>{dni}</TableCell>
+                </TableRow>
+              ))}
           </TableBody>
         </Table>
       </TableContainer>
--- src/service/passagerServices.ts
+++ src/service/passagerServices.ts
@@ -1,9 +1,23 @@
+import { Passager } from '@/types'
 import { api } from './api'
 
 export const getPassagers = async () => {
   try {
     const response = await api.get('/passagers')
     const data = (await response?.data.data) || []
+
+    return data
+  } catch (error) {
+    console.error('Error:', error)
+    return []
+  }
+}
+
+export const createPassager = async (passager: Passager) => {
+  try {
+    const response = await api.post('/passagers', passager)
+    const data = (await response?.data.data) || []
+    console.log('***  ~ createPassager  ~ passagers data:', data)
 
     return data
   } catch (error) {
