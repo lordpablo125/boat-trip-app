e8d5cf08b806272ddb2d4f50b07914bc29b92010
pablo <pablo.diaz@clara.co>
2025-02-04 21:02:22 -0300

    Components removed from Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-import BoatCard from '@/components/BoatCard'
-import EmployeeList from '@/components/EmployeeList'
-import PassagersList from '@/components/PassagersList'
-
 export default async function Home() {
   return (
     <div className='container'>
-      {/* <BoatCard /> */}
-      {/* <EmployeeList /> */}
-      <PassagersList />
+      <h1>Home</h1>
     </div>
   )
 }
