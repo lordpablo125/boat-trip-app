a4de54f6d9e54cec0abafc07a41f84f004b78331
Pablo <lord.pablo125@gmail.com>
2025-03-05 12:13:49 -0300

    employee update

--- src/app/employees/page.tsx
+++ src/app/employees/page.tsx
@@ -1,10 +1,27 @@
 'use client'
-import EmployeeList from '@/components/EmployeeList'
-import { useGetEmployees } from '@/service/employeeServices'
+
+import CustomTableData from '@/components/CustomTableData'
+import { useDeleteEmployee, useGetEmployees } from '@/service/employeeServices'
 import React from 'react'
 
 const PageEmployeeList = () => {
-  return <EmployeeList useGetTableData={useGetEmployees} />
+  const columns = [
+    { key: 'id', label: 'Id' },
+    { key: 'name', label: 'Name' },
+    { key: 'role', label: 'Role' }
+  ]
+  const { mutate: deleteEmployee } = useDeleteEmployee({
+    onSuccess: () => window.location.reload()
+  })
+
+  return (
+    <CustomTableData
+      columns={columns}
+      addNewURI={'employees'}
+      useGetTableData={useGetEmployees}
+      deteleMutation={deleteEmployee}
+    />
+  )
 }
 
 export default PageEmployeeList
