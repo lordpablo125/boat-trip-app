4c7142a6ea5f29b69eccbd49e1cf3042bbbf3ad0
Pablo <lord.pablo125@gmail.com>
2025-03-11 10:52:30 -0300

    Boats card & service updated

--- src/components/BoatCard.tsx
+++ src/components/BoatCard.tsx
@@ -1,40 +1,57 @@
 'use client'
 import { Boat, BoatCardProps } from '@/types'
-import { Box, Card, CardContent, CardMedia, Typography } from '@mui/material'
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  CardMedia,
+  Typography
+} from '@mui/material'
+import Link from 'next/link'
 import { FC } from 'react'
+import AddIcon from '@mui/icons-material/Add'
 
 const BoatCard: FC<BoatCardProps> = ({ boats }) => {
   return (
     <Box className='flex flex-col items-center '>
-      <Typography variant='h6' component='h2'>
+      <Typography className='text-center' variant='h2'>
         Boats list
       </Typography>
+      <Link href={`/boats/create`} className=' my-4' passHref>
+        <Button color='primary' variant='outlined'>
+          <AddIcon />
+          Add new
+        </Button>
+      </Link>
       {boats?.length > 0 &&
         boats.map((boat: Boat) => (
-          <div key={boat.name}>
-            <Card className='mb-4' sx={{ width: 345, height: 400 }}>
-              <CardMedia
-                component='img'
-                className='h-[250px]'
-                alt={boat.name}
-                image={boat.image}
-              />
-              <CardContent>
-                <Typography gutterBottom variant='h5' component='div'>
-                  {boat.name}
-                </Typography>
-                <Typography variant='body2' sx={{ color: 'text.secondary' }}>
-                  Brand: {boat.brand}
-                </Typography>
-                <Typography variant='body2' sx={{ color: 'text.secondary' }}>
-                  model: {boat.model}
-                </Typography>
-                <Typography variant='body2' sx={{ color: 'text.secondary' }}>
-                  Maximum Capacity: {boat.maximum_capacity}
-                </Typography>
-              </CardContent>
-            </Card>
-          </div>
+          <Card
+            key={boat.name}
+            className='mb-4'
+            sx={{ width: 345, height: 400 }}
+          >
+            <CardMedia
+              component='img'
+              className='h-[250px]'
+              alt={boat.name}
+              image={boat.image}
+            />
+            <CardContent>
+              <Typography gutterBottom variant='h5' component='div'>
+                {boat.name}
+              </Typography>
+              <Typography variant='body2' sx={{ color: 'text.secondary' }}>
+                Brand: {boat.brand}
+              </Typography>
+              <Typography variant='body2' sx={{ color: 'text.secondary' }}>
+                model: {boat.model}
+              </Typography>
+              <Typography variant='body2' sx={{ color: 'text.secondary' }}>
+                Maximum Capacity: {boat.maximum_capacity}
+              </Typography>
+            </CardContent>
+          </Card>
         ))}
     </Box>
   )
--- src/service/boatServices.ts
+++ src/service/boatServices.ts
@@ -35,3 +35,86 @@ export const useGetBoats = (obj: object) => {
 
   return query
 }
+
+export const getEmployee = async (documentId: string) => {
+  try {
+    const response = await api.get(`/employees/${documentId}`)
+    const data = (await response?.data.data) || {}
+
+    return data
+  } catch (error) {
+    console.error('Error:', error)
+    return []
+  }
+}
+
+export const useGetEmployee = (documentId: string) => {
+  const { data } = useQuery({
+    queryKey: ['employee'],
+    queryFn: () => getEmployee(documentId)
+  })
+
+  return data
+}
+
+export const createEmployee = async (employee: Employee) => {
+  try {
+    const payload = { data: employee }
+    const response = await api.post('/employees', payload)
+    const data = (await response?.data.data) || []
+
+    return data
+  } catch (error) {
+    console.error('Error:', error)
+    return {}
+  }
+}
+
+export const useCreateEmployee = () =>
+  useMutation({
+    mutationFn: createEmployee
+  })
+
+export const editEmployee = async ({
+  documentId,
+  employee
+}: {
+  documentId: DocumentId
+  employee: Employee
+}) => {
+  try {
+    const payload = { data: employee }
+    const response = await api.put(`/employees/${documentId}`, payload)
+    const data = (await response?.data.data) || []
+
+    return data
+  } catch (error) {
+    console.error('Error:', error)
+    return []
+  }
+}
+
+export const useEditEmployee = () => {
+  return useMutation({
+    mutationFn: editEmployee
+  })
+}
+
+export const deleteEmployee = async (id: Id) => {
+  try {
+    const response = await api.delete(`/employees/${id}`)
+    const data = (await response?.data.data) || []
+
+    return data
+  } catch (error) {
+    console.error('Error:', error)
+    return []
+  }
+}
+
+export const useDeleteEmployee = (params: object) => {
+  return useMutation({
+    ...params,
+    mutationFn: deleteEmployee
+  })
+}
